11bf8fcedaa849de2b402f20f5619710aae7fc4f
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-09-15 19:03:36 -0400

    created a function
    created a function to do the math and added a service charge

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+SERVICE_CHARGE = 2
 TICKET_PRICE =10
 tickets_remaining = 100
+
+# create calculate_price function. It takes tickets and returns TICKET_PRICE * tickets
+
+def calculate_price(number_of_tickets):
+    # add the service charge to our result
+    return TICKET_PRICE * number_of_tickets + SERVICE_CHARGE
 
 while tickets_remaining:
     print("There are {} tickets remaining".format(tickets_remaining))
@@ -12,10 +19,11 @@ while tickets_remaining:
     except ValueError as err:
         print("oh no! we ran into an issue. {} please try again".format(err))
     else:
-        total_cost = TICKET_PRICE * tickets
+        total_cost = calculate_price(tickets)
         print("Your tickets cost only {}".format(total_cost))
         proceed = input("Do you want to proceed? /n (Enter yes/no)")
         if proceed == "yes":
+            # To do: gather credit card info and process it
             print("SOLD!!")
             tickets_remaining -= tickets
         else:
